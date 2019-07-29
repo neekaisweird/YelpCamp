5794546ba26cdef2201f17f6c4f6d86b1ea7d731
neeka <neekaishappy@gmail.com>
2019-07-28 18:52:42 -0700

    deleted old db url

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var commentRoutes = require("./routes/comments"),
 	indexRoutes = require("./routes/index");
 
 mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
-// mongoose.connect("mongodb+srv://neeka:Moose21@cluster0-luoes.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
